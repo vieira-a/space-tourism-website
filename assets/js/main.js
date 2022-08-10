357b18a283b30a44a93c72e69770ccb36face9ca
Anderson Vieira <asvieira.br@gmail.com>
2022-08-09 23:56:52 -0300

    Ajuste CSS menu mobile

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+const openMenu = document.getElementsByClassName("open-menu");
+
+function openMobileMenu() {
+    document.body.classList.add("menu-expanded")
+}
+
+function closeMobileMenu() {
+    document.body.classList.remove("menu-expanded")
+}
